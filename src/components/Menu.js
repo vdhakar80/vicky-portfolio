7a01db7be92f3c364854d67cbd2571b15f552a35
vdhakar80 <vdhakar80@gmail.com>
2022-04-30 19:01:00 +0530

    feat: Menu added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "./../styles/menu.css";
 
 function Menu() {
   return (
-    <div>
+    <div className="menu">
       <div>
-        <img src="" />
+        <center>
+          <img
+            className="profile-picture"
+            src="https://upload.wikimedia.org/wikipedia/en/e/e1/Doraemon_2015.jpg"
+            alt="profile"
+          />
+          <h3>Vicky Dhakar</h3>
+          <h4>Full Stack Developer</h4>
+          <h5>Home</h5>
+          <h5>About</h5>
+          <h5>Services</h5>
+          <h5>Skills</h5>
+          <h5>Education</h5>
+          <p>
+            © Copyright ©2022 All rights reserved | This template is made with
+            by Muskan Bandil and Vicky Dhakar
+          </p>
+        </center>
       </div>
     </div>
   );
