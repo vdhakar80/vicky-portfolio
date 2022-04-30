71e903e110528997acff55de653d1075df14ec6a
vdhakar80 <vdhakar80@gmail.com>
2022-04-30 17:15:53 +0530

    feat: empty files added

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Details from "./Details";
+import Menu from "./Menu";
+
+function Home() {
+  return (
+    <div>
+      <div>
+        <Menu />
+      </div>
+      <div>
+        <Details />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
