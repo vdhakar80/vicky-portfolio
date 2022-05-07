08589e5248df041d24baf0aa13fed0ba97128305
vdhakar80 <vdhakar80@gmail.com>
2022-05-07 21:12:14 +0530

    feat: about section layout

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,23 @@ function About(props) {
   const { componentStartRef } = props;
   return (
     <div ref={componentStartRef} className="about">
-      About
+      <div className="about-heading">Who am i?</div>
+
+      <p className="about-para">
+        {`Hi I'm Jackson Ford On her way she met a copy. The copy warned the Little Blind T`}
+      </p>
+      <p>
+        {
+          "Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar."
+        }
+      </p>
+      <div className="about-cards">
+        {["Graphic Design", "Web Design", "Software", "Application"].map(
+          (data) => {
+            return <div className="about-card">{data}</div>;
+          }
+        )}
+      </div>
     </div>
   );
 }
