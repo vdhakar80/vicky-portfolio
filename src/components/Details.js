8630bfce85d7a29cf6b18734539c2c6dbbfab8e6
muskan <muskanbandil@gmail.com>
2022-05-08 16:18:44 +0530

    feat:certifications component

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,6 +5,7 @@ import Education from "./Education";
 import Skills from "./Skills";
 import Home from "./Home";
 import Experience from "./Experience";
+import Certifications from "./Certifications";
 
 function Details(props) {
   const { componentStartRef, currentComponent } = props;
@@ -34,6 +35,11 @@ function Details(props) {
       <Experience
         componentStartRef={
           currentComponent === "Experience" ? componentStartRef : null
+        }
+      />
+      <Certifications
+        componentStartRef={
+          currentComponent === "Certifications" ? componentStartRef : null
         }
       />
     </div>
